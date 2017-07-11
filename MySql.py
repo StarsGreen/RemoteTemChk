fa1797e4a282ae641aa2581825865dfa44779ab7
root <root@raspberrypi>
2017-07-11 14:21:06 +0800

    17-7-11

--- MySql.py
+++ MySql.py
@@ -21,20 +21,20 @@ def MySqlWrite(data1,data2,data3):
 		print "connetion error!"
 	db.close();
 #	pdb.set_trace()
-	db2 = MySQLdb.connect(host="60.205.185.77",port=3306,user='worker',passwd='302518',db='TemChk')
+#	db2 = MySQLdb.connect(host="60.205.185.77",port=3306,user='worker',passwd='302518',db='TemChk')
 #	pdb.set_trace()
-	cursor2 = db2.cursor()
+#	cursor2 = db2.cursor()
 #	current_time=time.ctime()
 #	sql2='update data_realtime set data1="%s",set data2="%s",set data3="%s" where id = 1' % (data1,data2,data3)
-	sql2='update data_realtime set data1="%s",data2="%s",data3="%s" where id=1'% (data1,data2,data3)
+#	sql2='update data_realtime set data1="%s",data2="%s",data3="%s" where id=1'% (data1,data2,data3)
 #	pdb.set_trace()
-	try:
-		cursor2.execute(sql2);
-		db2.commit();
-	except:
-		pdb.set_trace()
-		flag=0;
-		db2.rollback();
-		print "connetion error!"
-	db2.close();
-	return flag;
+#	try:
+#		cursor2.execute(sql2);
+#		db2.commit();
+#	except:
+#		pdb.set_trace()
+#		flag=0;
+#		db2.rollback();
+#		print "connetion error!"
+#	db2.close();
+#	return flag;
--- main.py
+++ main.py
@@ -8,12 +8,16 @@ import MySql
 import pdb
 if __name__ == '__main__':
 	while 1:
+		print time.ctime()
 		tem=ReadTem.ReadTem()
 #		print tem[0],tem[1],tem[2]
 #		pdb.set_trace()
-		if MySql.MySqlWrite(tem[0],tem[1],tem[2])!=1:
-			pdb.set_trace()
-			print "write data to server error!"
+		MySql.MySqlWrite(tem[0],tem[1],tem[2])
+#			pdb.set_trace()
+#			print "write data to server error!"
 		WriteTem.WriteTem(tem[0],tem[1],tem[2])
-#		time.sleep(0.5)		
-			 		
+#		time.sleep(0.5)	
+#		pdb.set_trace()	
+		print time.ctime()
+#		pdb.set_trace()	
+	 		
